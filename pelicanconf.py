0f624dc13695c23f893b730ede1dce40dca62ad8
Paulo Oliveira <paulo.oliveira@corp.globo.com>
2017-07-15 11:01:04 -0300

    update pelicanconfig with my theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,13 +6,9 @@ AUTHOR = u'Paulo Oliveira'
 SITENAME = u'Um dev qualquer || Paulo Oliveira'
 SITEURL = ''
 # SITEURL = 'https://umdevqualquer.com.br'
-THEME = u'./themes/pelican-elegant'
-
 
 PATH = 'content'
-
 TIMEZONE = u'America/Sao_Paulo'
-
 DEFAULT_LANG = u'pt_BR'
 
 # Feed generation is usually not desired when developing
@@ -24,17 +20,17 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = ()
-# LINKS = (('Pelican', 'http://getpelican.com/'),
-#          ('Python.org', 'http://python.org/'),
-#          ('Jinja2', 'http://jinja.pocoo.org/'),
-#          ('You can modify those links in your config file', '#'),)
+# LINKS = (('Pelican', 'http://getpelican.com/'),)
 
 # Social widget
 SOCIAL = ()
 # SOCIAL = (('github', '#'),
 #           ('linkedin', '#'),)
 
-# DEFAULT_PAGINATION = 10
-
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
+SLUGIFY_SOURCE = 'title'
+THEME = u'./themes/made_by_designer'
+DEFAULT_CATEGORY = 'Sem categoria'
+THEME_STATIC_URL = u'/theme'
+# THEME_STATIC_URL = THEME + u'/static'
