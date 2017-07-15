7d2e18e2af439e68c3e33d762f8c240cb6531a73
Paulo Oliveira <paulo.oliveira@corp.globo.com>
2017-07-15 10:42:01 -0300

    FIRST COMMIT

---
+++ README.md
@@ -0,0 +1,7 @@
+# [Um dev qualquer](https://umdevqualquer.com.br) | ~paulopotter.github.io~
+---
+
+Meu blog pessoal onde eu guardo dicas, tutoriais e um pouco de opinião.
+
+Blog focado no desenvolvimento full-stack, com um pé mais puxado ao front-end.
+
---
+++ pelicanconf.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Paulo Oliveira'
+SITENAME = u'Um dev qualquer || Paulo Oliveira'
+SITEURL = ''
+# SITEURL = 'https://umdevqualquer.com.br'
+THEME = u'./themes/pelican-elegant'
+
+
+PATH = 'content'
+
+TIMEZONE = u'America/Sao_Paulo'
+
+DEFAULT_LANG = u'pt_BR'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = ()
+# LINKS = (('Pelican', 'http://getpelican.com/'),
+#          ('Python.org', 'http://python.org/'),
+#          ('Jinja2', 'http://jinja.pocoo.org/'),
+#          ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = ()
+# SOCIAL = (('github', '#'),
+#           ('linkedin', '#'),)
+
+# DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
