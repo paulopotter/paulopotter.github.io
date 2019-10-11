360dfe80db4fc90c6a585279fd72d47d6aa740ea
Paulo Oliveira <paulofrauches@gmail.com>
2019-10-11 14:33:44 -0300

    update summary plugin

--- publishconf.py
+++ publishconf.py
@@ -14,7 +14,8 @@ SITEURL = 'https://umdevqualquer.com.br'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+
 
 DELETE_OUTPUT_DIRECTORY = True
 
