b4b7afcbc94bf01fb8b922bef8d8283db4a29091
Paulo Oliveira <paulofrauches@gmail.com>
2022-07-03 11:56:30 -0300

    fix(?): tag manager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from 'next/script';
 import  dayjs from "dayjs";
 import 'dayjs/locale/pt-br';
 
@@ -31,19 +30,24 @@ const Document = () => {
         href="https://fonts.googleapis.com/css2?family=Bitter:ital,wght@0,400;1,700&family=Montserrat:wght@400;700&display=swap"
         rel="stylesheet"
       />
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_CODE}`}
-        strategy="afterInteractive"
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+            ga('create', "${GA_CODE}", 'auto');
+            ga('send', 'pageview');
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', ${GA_CODE});
+        `,
+        }}
       />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', ${GA_CODE});
-        `}
-      </Script>
+      {/* @ts-ignore */}
+      <script async={true} src={`https://www.googletagmanager.com/gtag/js?id=${GA_CODE}`} strategy="afterInteractive" />
     </Html>
   );
 };
