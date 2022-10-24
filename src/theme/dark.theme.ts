4cbea61fa8358d9600b581b3c6b92b4ace11eea4
Paulo Oliveira <paulofrauches@gmail.com>
2022-10-24 07:42:18 -0300

    style: move colors from tokens to theme

--- src/theme/dark.theme.ts
+++ src/theme/dark.theme.ts
@@ -1,42 +1,55 @@
-import { ITHEME } from './theme.types';
+import { ITHEME, COLOR } from './theme.types';
 import * as TOKENS from './tokens';
+
+const COLOR_ONYX: COLOR = '#32373D';
+const COLOR_LIGHT_SILVER: COLOR = '#D6D7D8';
+const COLOR_CULTURED: COLOR = '#F5F5F5';
+const COLOR_SILVER_SAND: COLOR = '#C1C3C4';
+const COLOR_SILVER_SAND_ALPHA_70: COLOR = '#rgba(193, 195, 196, .7)';
+const COLOR_DARK_JUNGLE_GREEN: COLOR = '#1e2124';
+const COLOR_MEDIUM_SKY_BLUE: COLOR = '#79DDF5';
+const COLOR_MEDIUM_SKY_BLUE_ALPHA_10: COLOR = 'rgba(121, 221, 245, .1)';
+const COLOR_MEDIUM_SKY_BLUE_ALPHA_20: COLOR = 'rgba(121, 221, 245, .2)';
+const COLOR_MEDIUM_SKY_BLUE_ALPHA_50: COLOR = 'rgba(121, 221, 245, .5)';
+const COLOR_BEGONIA: COLOR = '#FF7171';
+// const COLOR_MEDIUM_SKY_BLUE_ALPHA_70: COLOR = 'rgba(121, 221, 245, .7)';
 
 export const THEME_DARK: ITHEME = {
   name: 'dark',
-  background: TOKENS.COLOR_ONYX,
-  border: TOKENS.COLOR_MEDIUM_SKY_BLUE_ALPHA_50,
+  background: COLOR_ONYX,
+  border: COLOR_MEDIUM_SKY_BLUE_ALPHA_50,
   boxShadow: TOKENS.COLOR_BLACK_ALPHA_10,
   button: {
-    background: TOKENS.COLOR_DARK_JUNGLE_GREEN,
-    color: TOKENS.COLOR_MEDIUM_SKY_BLUE,
+    background: COLOR_DARK_JUNGLE_GREEN,
+    color: COLOR_MEDIUM_SKY_BLUE,
   },
   card: {
-    background: TOKENS.COLOR_DARK_JUNGLE_GREEN,
+    background: COLOR_DARK_JUNGLE_GREEN,
   },
   code: {
     background: TOKENS.COLOR_WHITE_ALPHA_10,
-    color: TOKENS.COLOR_BEGONIA,
+    color: COLOR_BEGONIA,
   },
   color: {
-    primary: TOKENS.COLOR_SILVER_SAND,
-    secondary: TOKENS.COLOR_LIGHT_SILVER,
-    tertiary: TOKENS.COLOR_SILVER_SAND_ALPHA_70,
+    primary: COLOR_SILVER_SAND,
+    secondary: COLOR_LIGHT_SILVER,
+    tertiary: COLOR_SILVER_SAND_ALPHA_70,
   },
-  link: TOKENS.COLOR_MEDIUM_SKY_BLUE,
-  outline: TOKENS.COLOR_MEDIUM_SKY_BLUE,
-  related: TOKENS.COLOR_SILVER_SAND_ALPHA_70,
+  link: COLOR_MEDIUM_SKY_BLUE,
+  outline: COLOR_MEDIUM_SKY_BLUE,
+  related: COLOR_SILVER_SAND_ALPHA_70,
   table: {
     header: {
       background: {
-        primary: TOKENS.COLOR_MEDIUM_SKY_BLUE_ALPHA_50,
-        secondary: TOKENS.COLOR_ONYX,
+        primary: COLOR_MEDIUM_SKY_BLUE_ALPHA_50,
+        secondary: COLOR_ONYX,
       },
-      color: TOKENS.COLOR_CULTURED,
+      color: COLOR_CULTURED,
     },
     body: {
       background: {
-        primary: TOKENS.COLOR_MEDIUM_SKY_BLUE_ALPHA_20,
-        secondary: TOKENS.COLOR_MEDIUM_SKY_BLUE_ALPHA_10,
+        primary: COLOR_MEDIUM_SKY_BLUE_ALPHA_20,
+        secondary: COLOR_MEDIUM_SKY_BLUE_ALPHA_10,
       },
     },
   },
--- src/theme/light.theme.ts
+++ src/theme/light.theme.ts
@@ -1,42 +1,52 @@
-import { ITHEME } from './theme.types';
+import { ITHEME, COLOR } from './theme.types';
 import * as TOKENS from './tokens';
+
+const COLOR_CULTURED: COLOR = '#F5F5F5';
+const COLOR_ELETRIC_INDIGO: COLOR = '#6F11FB';
+const COLOR_ELETRIC_INDIGO_ALPHA_10: COLOR = 'rgba(111, 17, 251, .1)';
+const COLOR_ELETRIC_INDIGO_ALPHA_20: COLOR = 'rgba(111, 17, 251, .2)';
+const COLOR_ELETRIC_INDIGO_ALPHA_50: COLOR = 'rgba(111, 17, 251, .5)';
+const COLOR_ONYX_LIGHT: COLOR = '#363D42';
+const COLOR_ONYX_LIGHT_ALPHA_70: COLOR = 'rgba(54, 61, 66, .7)';
+const COLOR_VIOLET: COLOR = '#9700A1';
+const COLOR_GOLDEN_GATE_BRIDGE: COLOR = '#B73333';
 
 export const THEME_LIGHT: ITHEME = {
   name: 'light',
-  background: TOKENS.COLOR_CULTURED,
-  border: TOKENS.COLOR_VIOLET,
+  background: COLOR_CULTURED,
+  border: COLOR_VIOLET,
   boxShadow: TOKENS.COLOR_BLACK_ALPHA_10,
   button: {
     background: TOKENS.COLOR_WHITE,
-    color: TOKENS.COLOR_ELETRIC_INDIGO,
+    color: COLOR_ELETRIC_INDIGO,
   },
   card: {
     background: TOKENS.COLOR_WHITE,
   },
   code: {
     background: TOKENS.COLOR_BLACK_ALPHA_10,
-    color: TOKENS.COLOR_GOLDEN_GATE_BRIDGE,
+    color: COLOR_GOLDEN_GATE_BRIDGE,
   },
   color: {
     primary: TOKENS.COLOR_BLACK,
-    secondary: TOKENS.COLOR_ONYX_LIGHT,
-    tertiary: TOKENS.COLOR_ONYX_LIGHT_ALPHA_70,
+    secondary: COLOR_ONYX_LIGHT,
+    tertiary: COLOR_ONYX_LIGHT_ALPHA_70,
   },
-  link: TOKENS.COLOR_ELETRIC_INDIGO,
-  outline: TOKENS.COLOR_ELETRIC_INDIGO,
-  related: TOKENS.COLOR_ONYX_LIGHT_ALPHA_70,
+  link: COLOR_ELETRIC_INDIGO,
+  outline: COLOR_ELETRIC_INDIGO,
+  related: COLOR_ONYX_LIGHT_ALPHA_70,
   table: {
     header: {
       background: {
-        primary: TOKENS.COLOR_ELETRIC_INDIGO_ALPHA_50,
-        secondary: TOKENS.COLOR_CULTURED,
+        primary: COLOR_ELETRIC_INDIGO_ALPHA_50,
+        secondary: COLOR_CULTURED,
       },
       color: TOKENS.COLOR_WHITE,
     },
     body: {
       background: {
-        primary: TOKENS.COLOR_ELETRIC_INDIGO_ALPHA_20,
-        secondary: TOKENS.COLOR_ELETRIC_INDIGO_ALPHA_10,
+        primary: COLOR_ELETRIC_INDIGO_ALPHA_20,
+        secondary: COLOR_ELETRIC_INDIGO_ALPHA_10,
       },
     },
   },
